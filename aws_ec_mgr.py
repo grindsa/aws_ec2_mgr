7b85a6b57259441f2c742340d8d78af2bab81d17
grindsa <grindelsack@gmail.com>
2025-09-22 07:41:27 +0200

    [fix] instance name added

--- aws_ec_mgr.py
+++ aws_ec_mgr.py
@@ -4,41 +4,66 @@ import sys
 import time
 import boto3
 
-if __name__ == '__main__':
+try:
+    from wa_hack_cli import simple_send
+except ImportError:
+    def simple_send(srv, port, destination, message):
+        print("Not supported")
 
-    parser = argparse.ArgumentParser(description='Start or stop ec2 instances')
-    parser.add_argument('-a', '--action', help='start or stop')
-    parser.add_argument('-r', '--region', help='region')
-    parser.add_argument('-i', '--instance_id', help='instance id')
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description="Start or stop ec2 instances")
+    parser.add_argument("-a", "--action", help="start or stop")
+    parser.add_argument("-r", "--region", help="region")
+    parser.add_argument("-i", "--instance_id", help="instance id")
     args = parser.parse_args()
     action = args.action
     region = args.region
     instance_id = args.instance_id
 
-    ec2 = boto3.resource('ec2', region_name=region)
+    WA_SRV = "192.168.123.38"
+    WA_PORT = 9009
+    WA_DESTINATION = "491717626871"
+    WA_MESSAGE = f'*AWS Status Overview* - {time.strftime("%d.%m.%Y %H:%M")}\n'
 
-    if action == 'start':
+
+    ec2 = boto3.resource("ec2", region_name=region)
+
+    if action == "start":
         ec2.instances.filter(InstanceIds=[instance_id]).start()
-        print('starting instance', instance_id)
-    elif action == 'stop':
+        print("starting instance", instance_id)
+    elif action == "stop":
         ec2.instances.filter(InstanceIds=[instance_id]).stop()
-        print('stopping instance', instance_id)
-    elif action in ['status', 'state']:
+        print("stopping instance", instance_id)
+    elif action in ["status", "state"]:
         instance = ec2.Instance(instance_id)
-        print(instance.state['Name'])
-    elif action == 'public_ip':
+        print(instance.state["Name"])
+    elif action == "public_ip":
         instance = ec2.Instance(instance_id)
         print(instance.public_ip_address)
-    elif action == 'private_ip':
+    elif action == "private_ip":
         instance = ec2.Instance(instance_id)
         print(instance.private_ip_address)
-    elif action == 'public_dns':
+    elif action == "public_dns":
         instance = ec2.Instance(instance_id)
-        print(instance.public_dns_name)
-    elif action == 'status_all':
+        print(i.tags[0]["Value"], instance.public_dns_name)
+    elif action == "list":
         instances = ec2.instances.all()
         for i in instances:
-            print(i.id, i.state['Name'], i.public_ip_address)
+            WA_MESSAGE = f"{WA_MESSAGE}{i.tags[0]['Value']} {i.id} {i.state['Name']} {i.public_ip_address}\n"
+        simple_send(WA_SRV, WA_PORT, WA_DESTINATION, WA_MESSAGE)
+    elif action == "status_all":
+        instances = ec2.instances.all()
+
+        for i in instances:
+            print(
+                i.tags[0]["Value"],
+                i.id,
+                i.state["Name"],
+                i.instance_type,
+                i.public_ip_address,
+            )
     else:
-        print('invalid action')
+        print("invalid action")
         sys.exit(1)
